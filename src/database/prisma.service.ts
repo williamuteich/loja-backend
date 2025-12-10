d703f6e74b72101f2e8e6d4113d36bb4b013a34e
william <williamuteich14@gmail.com>
2025-12-10 19:35:03 -0300

    atualizando prisma para versao 7

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,8 +1,15 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    $disconnect: any;
+    constructor() {
+        const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL || 'file:./dev.db' });
+        super({ adapter });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
