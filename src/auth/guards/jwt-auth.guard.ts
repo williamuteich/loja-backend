91a14f569069bf37c1dd5659fc549364a4667d8a
william <williamuteich14@gmail.com>
2025-12-20 22:41:00 -0300

    add: add funcionalidade de remover cookies do frontend quando jtw for invalido

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthErrors } from '../../common/errors/app-errors';
+import type { Response } from 'express';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
     handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
         if (err || !user) {
+            const http = context.switchToHttp();
+            const res = http.getResponse<Response>();
+
+            if (res && typeof res.clearCookie === 'function') {
+                res.clearCookie('access_token');
+            }
+
             if (err) {
                 throw err;
             }
-            throw AuthErrors.accessDenied('Access denied');
+
+            throw AuthErrors.userNotAuthenticated();
         }
 
         return user;
